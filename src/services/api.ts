addfa7917dab0a1e062f90a6e369e6e8098e827b
HassanMahmoud-dev <hmido2622@gmail.com>
2026-02-21 16:57:54 +0200

    add .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@ import axios, { type AxiosResponse } from 'axios'
 import swalService from './swal.service'
 import type { BaseResponse } from '@/types/api'
 
-export const API_URL = 'http://192.168.1.13:5000/api'
-export const BASE_URL = 'http://192.168.1.13:5000'
+export const API_URL = import.meta.env.VITE_API_URL
+export const BASE_URL = import.meta.env.VITE_BASE_URL
 
 const api = axios.create({
   baseURL: API_URL,
